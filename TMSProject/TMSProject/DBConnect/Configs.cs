cf5b89ed3bf4bf51b6f7fb9c7f27c3fe0eb07186
Mark <tnguyen5130@conestogac.on.ca>
2019-10-25 14:35:39 -0400

    Login Validation and New Order Screen (#1)
    Finish the login validaiton and set up the material design

---
+++ TMSProject/TMSProject/DBConnect/Configs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMSProject.DBConnect
+{
+    class Configs
+    {
+		public static string dbServer = "127.0.0.1";
+		public static string dbPort = "3306";
+		public static string dbUID = "root";
+		public static string dbPassword = "Cc8635130";
+		public static string dbDatabase = "tms";
+
+	}
+}
