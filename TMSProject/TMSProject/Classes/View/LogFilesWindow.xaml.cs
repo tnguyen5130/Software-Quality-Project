ae9010d750494186eb7a1d008e6dac07ae408d18
Abdlhince <56037656+Abdlhince@users.noreply.github.com>
2019-11-20 15:32:31 -0500

    Add admin xaml and cs file

---
+++ TMSProject/TMSProject/Classes/View/LogFilesWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TMSProject.Classes.View
+{
+    /// <summary>
+    /// Interaction logic for LogFilesWindow.xaml
+    /// </summary>
+    public partial class LogFilesWindow : UserControl
+    {
+        public LogFilesWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
