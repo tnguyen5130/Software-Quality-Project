3da7b9837a242af1a498951fe993573bc03bbee5
Jarvis <trungnguyen10051998@gmail.com>
2019-11-20 15:31:47 -0500

    Load DataGrid successful

--- TMSProject/TMSProject/Classes/View/PlannerHome.xaml.cs
+++ TMSProject/TMSProject/Classes/View/PlannerHome.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,10 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using TMSProject.Classes.Controller;
+using TMSProject.Classes.Model;
+using TMSProject.DBConnect;
+
 namespace TMSProject.Classes.View
 {
 	/// <summary>
@@ -20,14 +25,50 @@ namespace TMSProject.Classes.View
 	/// </summary>
 	public partial class PlannerHome : UserControl
 	{
-		public PlannerHome()
+        string ItemSelect;
+
+        public PlannerHome()
 		{
 			InitializeComponent();
+            loadTableData();            
 		}
 
-		public void btn_Load(object sender, RoutedEventArgs e)
+        private void loadTableData()
+        {
+            OrderBizDAO orderBiz = new OrderBizDAO();
+            orderBiz.loadOrderList(OrderList);
+        }
+
+        private void Row_DoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                if (OrderList.SelectedItem == null) return;
+                DataRowView dr = OrderList.SelectedItem as DataRowView;
+                DataRow newDr = dr.Row;
+                //MessageBox.Show(Convert.ToString(newDr.ItemArray[1]));
+                ItemSelect = Convert.ToString(newDr.ItemArray[1]);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        public void btn_Confirm(object sender, RoutedEventArgs e)
 		{
-
+            txtMessage.Text = "Your OrderID selected is: " + ItemSelect;
 		}
+
+        public void btn_Proceed(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void btn_Cancel(object sender, RoutedEventArgs e)
+        {
+
+        }
 	}
 }
